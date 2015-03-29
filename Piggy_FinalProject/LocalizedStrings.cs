587075e91d520ce1dea4bb087c548f1113dde900
Dat Huynh <badat0202@yahoo.com.vn>
2015-03-29 23:04:55 +0700

    Đạt Huỳnh Initialize github
    Share code cho anh em

---
+++ Piggy_FinalProject/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Piggy_FinalProject.Resources;
+
+namespace Piggy_FinalProject
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
